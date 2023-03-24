b698fca409256ca20625bc4d19c305a8ba7c1e3e
andresantosr <andresanro03@gmail.com>
2023-03-23 22:10:07 -0300

    lab2 concluido

--- src/Controle.java
+++ src/Controle.java
@@ -1,25 +1,27 @@
 public class Controle {
     public static void main(String args[]){
-        Cliente pessoa = new Cliente("dezin", "710-735-301.24", "03/11/2003", "20", "RES. piramidde");
-        System.out.println(pessoa.getCPF());
+        Cliente pessoa = new Cliente("Joao", "852.678.860-41", "01/01/2000", "20", "Rua das Rosas");
+        System.out.printf("cpf: %s\n",pessoa.getCPF());
+        System.out.printf("o cpf é %b\n",pessoa.validateCPF(pessoa.getCPF()));
         pessoa.setCPF("11111111111111111");
-        System.out.println(pessoa.validateCPF("11111111111111111"));
+        System.out.printf("cpf: %s\n",pessoa.getCPF());
+        System.out.printf("o cpf é %b\n",pessoa.validateCPF("11111111111111111"));
         System.out.println(pessoa.toString());
         
-        Seguradora seg = new Seguradora("dezin", "62996973007", "dezin@gmail.com", "Res. Piramide");
-        System.out.println(seg.getTelefone());
+        Seguradora seg = new Seguradora("Joao", "61996973007", "joao@gmail.com", "Rua das Rosas");
+        System.out.printf("telefone: %s\n",seg.getTelefone());
         seg.setTelefone("456456456456");
-        System.out.println(seg.getTelefone());
+        System.out.printf("telefone: %s\n",seg.getTelefone());
         
-        Sinistro sin = new Sinistro("23/03/2023", "Res Piramide");
-        System.out.println(sin.getEndereco());
-        sin.setEndereco("OXXO");
-        System.out.println(sin.getEndereco());
+        Sinistro sin = new Sinistro("23/03/2023", "Rua das Rosas");
+        System.out.printf("endereco: %s\n",sin.getEndereco());
+        sin.setEndereco("Rua dos Girassois");
+        System.out.printf("endereco: %s\n",sin.getEndereco());
 
-        Veiculo vei = new Veiculo("ABC2325", "MERCEDES","C180");
-        System.out.println(vei.getModelo());
-        vei.setModelo("fusca");
-        System.out.println(vei.getModelo());
+        Veiculo vei = new Veiculo("ABC1234", "AUDI","Q8");
+        System.out.printf("modelo: %s\n",vei.getModelo());
+        vei.setModelo("Fusca");
+        System.out.printf("modelo: %s\n",vei.getModelo());
 
         
     }
